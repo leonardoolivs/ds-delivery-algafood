b8fc44541c8a368fb5506dd4359a3e41117b1728
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-04 20:41:20 -0300

    feat: Cria entity de Restaurante

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Restaurante.java
@@ -0,0 +1,24 @@
+package com.algaworks.dsdeliveryalgafood.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_RESTAURANTE")
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal taxaFrete;
+}
