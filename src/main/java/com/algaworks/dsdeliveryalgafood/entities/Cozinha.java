255e52aadbe42a214b95c485301e6716ee704ae5
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-04 20:41:46 -0300

    refactor: Adiciona Lombok a entity de Cozinha

--- src/main/java/com/algaworks/dsdeliveryalgafood/entities/Cozinha.java
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Cozinha.java
@@ -1,8 +1,14 @@
 package com.algaworks.dsdeliveryalgafood.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "TB_COZINHA")
 public class Cozinha {
 
