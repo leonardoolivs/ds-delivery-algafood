b019e5c965e4cdd05eeadbfe99b2ba039971a1f7
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-04 19:51:55 -0300

    build: Cria o projeto com Spring Data JPA, H2, Lombok e Spring Web

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/DsDeliveryAlgafoodApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.dsdeliveryalgafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDeliveryAlgafoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsDeliveryAlgafoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algaworks/dsdeliveryalgafood/DsDeliveryAlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.dsdeliveryalgafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDeliveryAlgafoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
