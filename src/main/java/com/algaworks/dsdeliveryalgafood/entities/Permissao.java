fc60a78098aeb79c14393d25e88e19ae8cd5ae62
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-05 21:00:44 -0300

    feat: Cria entity de Permissao

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Permissao.java
@@ -0,0 +1,22 @@
+package com.algaworks.dsdeliveryalgafood.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_PERMISSAO")
+public class Permissao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+}
