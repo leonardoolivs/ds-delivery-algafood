09d949c63fd485d70252a14d92f585d74f61f289
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-08 23:10:44 -0300

    fix: Muda a Cidade para lista em entity de Estado

--- src/main/java/com/algaworks/dsdeliveryalgafood/entities/Estado.java
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Estado.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -19,5 +21,5 @@ public class Estado {
     private String nome;
 
     @OneToMany(mappedBy = "estado")
-    private Cidade cidade;
+    private List<Cidade> cidade;
 }
