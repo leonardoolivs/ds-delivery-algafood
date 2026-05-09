a024b0b4ffdaeb88f0418e649dbfcaf6ef377e06
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-08 23:02:42 -0300

    feat: Adiciona anotacoes de Timestamp em Usuario para dataCadastro

--- src/main/java/com/algaworks/dsdeliveryalgafood/entities/Usuario.java
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Usuario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,6 +23,9 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
+
+    @CreationTimestamp
+    @Column(columnDefinition = "datetime")
     private LocalDateTime dataCadastro;
 
     @ManyToMany
