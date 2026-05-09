c81f9798bdf508b1f44998b4c54ceb8ca44a0f9a
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-09 19:22:56 -0300

    feat: Cria entity de Pedido

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Pedido.java
@@ -0,0 +1,29 @@
+package com.algaworks.dsdeliveryalgafood.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_PEDIDO")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal subtotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataConfirmacao;
+    private LocalDateTime dataCancelamento;
+    private LocalDateTime dataEntrega;
+}
