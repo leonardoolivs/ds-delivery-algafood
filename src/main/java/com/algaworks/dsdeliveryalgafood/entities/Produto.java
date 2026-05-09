8201060bdfb741a05ce3ca9fa044d7966c93bc96
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-08 22:53:43 -0300

    feat: Cria a entity de Produto

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Produto.java
@@ -0,0 +1,27 @@
+package com.algaworks.dsdeliveryalgafood.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_PRODUTO")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private BigDecimal preco;
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id")
+    private Restaurante restaurante;
+}
