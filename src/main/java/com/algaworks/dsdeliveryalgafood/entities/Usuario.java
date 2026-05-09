91fbda30fee39f2aba8762ece7aaeadea89cadda
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-08 23:02:01 -0300

    feat: Cria entidade de Usuario com relacionamento com Grupo

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Usuario.java
@@ -0,0 +1,32 @@
+package com.algaworks.dsdeliveryalgafood.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_USUARIO")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDateTime dataCadastro;
+
+    @ManyToMany
+    @JoinTable(name = "TB_USUARIO_GRUPOS",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<Grupo> grupos;
+}
