96426ec32b53dd47283c65836af71b6b8ba64a25
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-11 19:24:52 -0300

    feat: Cria Exception para EntityNotFound

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.algaworks.dsdeliveryalgafood.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
