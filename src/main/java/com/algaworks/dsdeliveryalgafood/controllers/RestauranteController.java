557edee37786f16f7b0d721d4eb6d47840a4807e
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-11 20:24:17 -0300

    fix: Ajuda CRUD

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/controllers/RestauranteController.java
@@ -0,0 +1,54 @@
+package com.algaworks.dsdeliveryalgafood.controllers;
+
+import com.algaworks.dsdeliveryalgafood.entities.Restaurante;
+import com.algaworks.dsdeliveryalgafood.services.CozinhaService;
+import com.algaworks.dsdeliveryalgafood.services.RestauranteService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurantes")
+@RequiredArgsConstructor
+public class RestauranteController {
+
+    private final RestauranteService service;
+
+    @PostMapping
+    public ResponseEntity<Restaurante> cadastrar(@RequestBody Restaurante restaurante){
+
+        restaurante = service.cadastrar(restaurante);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(restaurante.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(restaurante);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurante> buscarPorId(@PathVariable Long id){
+        Restaurante restaurante = service.buscarPorId(id);
+
+        return ResponseEntity.ok(restaurante);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Restaurante>> buscarTodos(){
+        List<Restaurante> restaurantes = service.buscarTodas();
+
+        return ResponseEntity.ok(restaurantes);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable Long id){
+        Restaurante restaurante = service.buscarPorId(id);
+
+        service.remover(restaurante.getId());
+    }
+
+}
