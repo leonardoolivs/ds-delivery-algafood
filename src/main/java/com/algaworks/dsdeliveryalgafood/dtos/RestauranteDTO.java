834a18ff83a1611db51b8f48f03258ad35f165cc
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-12 17:44:57 -0300

    feat: Cria DTO de Restaurante

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/dtos/RestauranteDTO.java
@@ -0,0 +1,18 @@
+package com.algaworks.dsdeliveryalgafood.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestauranteDTO {
+
+    private String nome;
+    private BigDecimal taxaFrete;
+    private Long cozinhaId;
+
+}
