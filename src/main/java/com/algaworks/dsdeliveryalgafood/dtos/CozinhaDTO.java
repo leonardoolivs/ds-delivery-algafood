2ceed4e14b069dba16bfab2c5ae61935e33e92b7
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-12 17:41:17 -0300

    feat: Cria DTO de Cozinha

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/dtos/CozinhaDTO.java
@@ -0,0 +1,14 @@
+package com.algaworks.dsdeliveryalgafood.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CozinhaDTO {
+
+    private String nome;
+
+}
