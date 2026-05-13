6db6519594888fb60ee70c82dcfaff7ffd20dc9c
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-13 19:28:07 -0300

    feat: Cria DTO de Cidade

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/dtos/CidadeDTO.java
@@ -0,0 +1,14 @@
+package com.algaworks.dsdeliveryalgafood.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CidadeDTO {
+
+    private Long id;
+    private String nome;
+}
