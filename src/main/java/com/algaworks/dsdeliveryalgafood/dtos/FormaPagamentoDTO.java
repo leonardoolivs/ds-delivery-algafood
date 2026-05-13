f3bccdcb569cad0cf71ae979e520d813251a5cb1
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-12 23:19:57 -0300

    feat: Cria DTo de FormaPagamento

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/dtos/FormaPagamentoDTO.java
@@ -0,0 +1,15 @@
+package com.algaworks.dsdeliveryalgafood.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FormaPagamentoDTO {
+
+    private Long id;
+    private String nome;
+
+}
