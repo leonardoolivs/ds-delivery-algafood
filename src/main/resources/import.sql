738120c1c03f1cd116d1544dea23902db5dbf842
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-13 22:13:26 -0300

    feat: Cria import.sql criando Cozinhas, Estados e Cidades

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+insert into TB_COZINHA (nome) values ('Brasileira');
+insert into TB_COZINHA (nome) values ('Japonesa');
+insert into TB_COZINHA (nome) values ('Indiana');
+insert into TB_COZINHA (nome) values ('Árabe');
+
+insert into TB_CIDADE (nome) values ('Vila Prudente');
+insert into TB_CIDADE (nome) values ('Complexo do Alemão');
+
+insert into TB_ESTADO (nome) values ('São Paulo');
+insert into TB_ESTADO (nome) values ('Rio de Janeiro');
