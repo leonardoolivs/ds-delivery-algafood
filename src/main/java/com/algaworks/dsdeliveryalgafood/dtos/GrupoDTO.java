288561c4ac67b2b73264b23b79d06d4332ffbe71
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-14 20:50:42 -0300

    feat: Cria DTO de Grupo

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/dtos/GrupoDTO.java
@@ -0,0 +1,15 @@
+package com.algaworks.dsdeliveryalgafood.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GrupoDTO {
+
+    private Long id;
+    private String nome;
+
+}
