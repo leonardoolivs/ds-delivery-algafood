c95f942f8835b0e67b9f010cb379ab2f39f851e4
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-13 22:03:24 -0300

    feat: Cria Service de Cidade com buscarPorId

--- src/main/java/com/algaworks/dsdeliveryalgafood/services/CidadeService.java
+++ src/main/java/com/algaworks/dsdeliveryalgafood/services/CidadeService.java
@@ -1,4 +1,22 @@
 package com.algaworks.dsdeliveryalgafood.services;
 
+import com.algaworks.dsdeliveryalgafood.entities.Cidade;
+import com.algaworks.dsdeliveryalgafood.exceptions.EntityNotFoundException;
+import com.algaworks.dsdeliveryalgafood.repositories.CidadeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CidadeService {
+
+    private final CidadeRepository repository;
+
+    @Transactional(readOnly = true)
+    public Cidade buscarPorId(Long id){
+        return repository.findById(id).orElseThrow(
+                () ->  new EntityNotFoundException("Cidade de ID " + id + " não existe"));
+    }
+
 }
