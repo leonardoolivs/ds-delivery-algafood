a5a9810d053eea631a17d1b3ace5f9fb65d8be8c
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-14 13:41:07 -0300

    feat: Cria Service de Usuario com o metodo Cadastrar

--- src/main/java/com/algaworks/dsdeliveryalgafood/services/UsuarioService.java
+++ src/main/java/com/algaworks/dsdeliveryalgafood/services/UsuarioService.java
@@ -1,14 +1,24 @@
 package com.algaworks.dsdeliveryalgafood.services;
 
+import com.algaworks.dsdeliveryalgafood.dtos.UsuarioDTO;
+import com.algaworks.dsdeliveryalgafood.entities.Usuario;
 import com.algaworks.dsdeliveryalgafood.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
 
     private final UsuarioRepository repository;
+    private final ModelMapper mapper;
 
-    public Usuario cadastrar(Usuari)
+    @Transactional
+    public Usuario cadastrar(UsuarioDTO dto){
+        Usuario usuario = mapper.map(dto, Usuario.class);
+
+        return repository.save(usuario);
+    }
 }
