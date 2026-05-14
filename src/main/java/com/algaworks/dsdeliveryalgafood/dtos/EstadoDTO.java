086f12dac29a964b037c5b72bd96ad53e8234f50
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-13 22:10:20 -0300

    feat: Cria DTO de Estado

---
+++ src/main/java/com/algaworks/dsdeliveryalgafood/dtos/EstadoDTO.java
@@ -0,0 +1,15 @@
+package com.algaworks.dsdeliveryalgafood.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EstadoDTO {
+
+    private Long id;
+    private String nome;
+
+}
