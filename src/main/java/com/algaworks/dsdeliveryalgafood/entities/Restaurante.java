92db3cb925e03c51773ddfcbaa948ce91343f7c6
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-15 01:55:18 -0300

    feat: Cria metodos de Adicionar e Remover formas de pagamento no Restaurante

--- src/main/java/com/algaworks/dsdeliveryalgafood/entities/Restaurante.java
+++ src/main/java/com/algaworks/dsdeliveryalgafood/entities/Restaurante.java
@@ -10,7 +10,9 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -46,10 +48,18 @@ public class Restaurante {
     @JoinTable(name = "TB_RESTAURANTE_FORMAS_PAGAMENTO",
             joinColumns = @JoinColumn(name = "restaurante_id"),
             inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+    private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
+
+    public void adicionarFormaPagamento(FormaPagamento formaPagamento){
+        this.formasPagamento.add(formaPagamento);
+    }
+
+    public void removerFormaPagamento(FormaPagamento formaPagamento){
+        this.formasPagamento.remove(formaPagamento);
+    }
 
     @PrePersist
     public void persist(){
